8c74c5671bee3a56e0b9400be4c0f60404c2edda
ArinaBanana08 <149187292+ArinaBanana08@users.noreply.github.com>
2023-11-24 12:27:52 -0500

    While loop

--- phythonBasics.py
+++ phythonBasics.py
@@ -1,15 +1,6 @@
-print ("Happy Halloween") 
-thisList = ["abc", 34, True, 40, "male"]
-print (thisList)
-print ("the length of this list ", len(thisList))
-name = input("What's your name?")
-print ("Hello, " + name + "!")
-num1 = float(input("enter first number"))
-num2 = float(input("Enter the second number"))
-total = num1 +num2
-print ("The sum is:", total)
-myList = ["Jack", "Jimmy",23, True, "Lady", 421]
-print (myList)
-print ("The length of my list :", len(myList))
-print ("Items from second list:", myList[-2])
-print ("The sum of i2 and i5 is ", myList[2] + myList[5])
+i = 1
+number = 0
+while i <= 100:
+   number += i
+   i += 1
+print (number)
