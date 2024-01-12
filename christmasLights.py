f9daabfa5e18dec191a8fbb9a1f2a0a418a2a073
ArinaBanana08 <149187292+ArinaBanana08@users.noreply.github.com>
2024-01-12 12:30:29 -0500

    loop

--- christmasLights.py
+++ christmasLights.py
@@ -2,3 +2,13 @@ def greet (name) :
     print ("Hello", name, "!")
 greet ("Alice")
 
+def displayName ():
+   name = str (input ("What is your name?"))
+   print (name)
+displayName () 
+
+def add_numbers (x, y):
+   sum result = x+y
+   print (sum_result)
+
+
