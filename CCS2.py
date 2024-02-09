0b0e625aa28d3880eed2111b923275f1252407b1
ArinaBanana08 <149187292+ArinaBanana08@users.noreply.github.com>
2024-02-09 12:32:40 -0500

    CCC

---
+++ CCS2.py
@@ -0,0 +1,9 @@
+
+n = int(input ("How many peppers do you want?"))
+
+pepperName = ["Poblano", "Mirasol", "Serrano", "Cayenne", "Thai", "Habereno"]
+heatUnit = [1500, 6000, 15500, 40000, 75000, 125000]
+
+for i in range (n) :
+    name = str (input ("Enter name : "))
+    if (name == pepperName [0]
